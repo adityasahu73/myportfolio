51d351e5eb235a30b68f11bf31aee008f5e1c145
Aditya Sahu <sahuaditya73@gmail.com>
2023-07-15 01:08:22 +0530

    added changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,6 +160,38 @@ buttons.forEach(btn1 => {
     form.addEventListener("submit", () => createToast(btn1.id));
 });
 
+// animation 
+
+var tl = gsap.timeline();
+
+tl.from('.navitem', {
+    stagger: .3,
+    duration: 2,
+    y: 20,
+    delay: 6.6,
+    ease: 'Expo.easeInOut',
+    opacity: 0
+})
+.from('#smline', {
+    width: 0,
+    duration: 1,
+    ease: 'Expo.easeInOut',
+}, '-=2')
+.from('.leftitem', {
+    stagger: .3,
+    duration: 2,
+    y: 20,
+    ease: 'Expo.easeInOut',
+    opacity: 0
+}, '-=2')
+.from('#right', {
+    duration: 2,
+    scale: 1.05,
+    ease: 'Expo.easeInOut',
+    opacity: 0
+}, '-=2')
+
+
 
 // Typed Js 
 

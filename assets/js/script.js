6c3e4bac09f87cca7b60050f698b9a98884cdc8f
Aditya Sahu <sahuaditya73@gmail.com>
2023-07-15 01:01:44 +0530

    added changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,3 +103,170 @@ if (localStorage.getItem("theme") === "light_theme") {
   document.body.classList.remove("light_theme");
   document.body.classList.add("dark_theme");
 }
+
+
+//  Google-sheets
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzwMYsz7xXfgPFk7ltMVmSJ-PYSUo71JqG6tb7M0K8tHUvH-sufJt6DHbJdSUJ1PhIW/exec'
+const form = document.forms['google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => createToast(btn1.id))
+    .catch(error => console.error('Error!', error.message))
+})
+
+
+// Toast popup
+
+const notifications = document.querySelector(".notifications"),
+buttons = document.querySelectorAll(".buttons .btn1");
+
+// Object containing details for different types of toasts
+const toastDetails = {
+    timer: 5000,
+    send: {
+        icon: 'fa-circle-check',
+        text: 'Success: This is a success toast.',
+    },
+}
+
+const removeToast = (toast) => {
+    toast.classList.add("hide");
+    if(toast.timeoutId) clearTimeout(toast.timeoutId); // Clearing the timeout for the toast
+    setTimeout(() => toast.remove(), 500); // Removing the toast after 500ms
+}
+
+const createToast = (id) => {
+    // Getting the icon and text for the toast based on the id passed
+    const { icon, text } = toastDetails[id];
+    const toast = document.createElement("li"); // Creating a new 'li' element for the toast
+    toast.className = `toast ${id}`; // Setting the classes for the toast
+    // Setting the inner HTML for the toast
+    toast.innerHTML = `<div class="column">
+                         <i class="fa-solid ${icon}"></i>
+                         <span>Your Message Successfully sent!</span>
+                      </div>
+                      <i class="fa-solid fa-xmark" onclick="removeToast(this.parentElement)"></i>`;
+    notifications.appendChild(toast); // Append the toast to the notification ul
+    // Setting a timeout to remove the toast after the specified duration
+    toast.timeoutId = setTimeout(() => removeToast(toast), toastDetails.timer);
+}
+
+// Adding a click event listener to each button to create a toast when clicked
+buttons.forEach(btn1 => {
+    form.addEventListener("submit", () => createToast(btn1.id));
+});
+
+
+// Typed Js 
+
+var typed = new Typed('#element', {
+  strings:['Frontend Developer', 'Web Develpoper','Web Designer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay:1000,
+  loop:true
+});
+
+
+// qual
+
+const tabs =document.querySelectorAll('[data-target]'),
+      tabsContents = document.querySelectorAll('[data-content]') 
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click',()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabsContents.forEach(tabContent=>{
+          tabContent.classList.remove('qual-active')
+        })
+        target.classList.add('qual-active')
+
+        tabs.forEach(tab=>{
+          tab.classList.remove('qual-active')
+        })
+        tab.classList.add('qual-active')
+    })  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Animation 
+
+var tl = gsap.timeline();
+
+tl.from('.navitem', {
+    stagger: .3,
+    duration: 2,
+    y: 20,
+    // delay: 6.5,
+    ease: 'Expo.easeInOut',
+    opacity: 0
+})
+.from('#smline', {
+    width: 0,
+    duration: 1,
+    ease: 'Expo.easeInOut',
+}, '-=2')
+.from('.leftitem', {
+    stagger: .3,
+    duration: 2,
+    y: 20,
+    ease: 'Expo.easeInOut',
+    opacity: 0
+}, '-=2')
+.from('#right', {
+    duration: 2,
+    scale: 1.05,
+    ease: 'Expo.easeInOut',
+    opacity: 0
+}, '-=2')
+
+
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+  if(entry.isIntersecting){
+        entry.target.classList.add("show-items");
+  }else{
+        entry.target.classList.remove("show-items");
+  }
+ });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
+
+const scrollRight = document.querySelectorAll(".scroll-right");
+scrollRight.forEach((el)=>observer.observe(el));
+
+const scrollLeft = document.querySelectorAll(".scroll-left");
+scrollLeft.forEach((el)=>observer.observe(el));

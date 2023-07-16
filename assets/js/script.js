1abcd9c938c12bb21b758eff2cad37dba847fbbe
Aditya Sahu <sahuaditya73@gmail.com>
2023-07-16 21:46:52 +0530

    added feedback

--- assets/js/script.js
+++ assets/js/script.js
@@ -109,57 +109,38 @@ if (localStorage.getItem("theme") === "light_theme") {
 //  Google-sheets
 
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzwMYsz7xXfgPFk7ltMVmSJ-PYSUo71JqG6tb7M0K8tHUvH-sufJt6DHbJdSUJ1PhIW/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycby0H5Vx5s3vKPCZg9JaMRiyVo_0ID_JCbObQeE1aKBZc6ZK9wWh2pGs9KiDfRkw8Am5/exec'
 const form = document.forms['google-sheet']
+const starform =document.querySelector('#star-form');
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
+starform.addEventListener('submit', (e) => {
+  let zz=new FormData(starform);
+  let sucessfull =document.getElementById('sucessfully');
+  e.target.submit.innerHTML="sending....";
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body:zz})
+    .then(response =>response.text())
+    .then((finalRes)=>{
+      e.target.submit.innerHTML="Send";
+      sucessfull.innerHTML="Thanks For rating!!"
+    })
+})
+
+form.addEventListener('submit', (e) => {
+  e.target.submit.innerHTML="sending....";
+  e.preventDefault();
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => createToast(btn1.id))
-    .catch(error => console.error('Error!', error.message))
+    .then(response =>{
+      e.target.submit.innerHTML="send";
+    })
 })
 
 
-// Toast popup
-
-const notifications = document.querySelector(".notifications"),
-buttons = document.querySelectorAll(".buttons .btn1");
-
-// Object containing details for different types of toasts
-const toastDetails = {
-    timer: 5000,
-    send: {
-        icon: 'fa-circle-check',
-        text: 'Success: This is a success toast.',
-    },
-}
-
-const removeToast = (toast) => {
-    toast.classList.add("hide");
-    if(toast.timeoutId) clearTimeout(toast.timeoutId); // Clearing the timeout for the toast
-    setTimeout(() => toast.remove(), 500); // Removing the toast after 500ms
-}
-
-const createToast = (id) => {
-    // Getting the icon and text for the toast based on the id passed
-    const { icon, text } = toastDetails[id];
-    const toast = document.createElement("li"); // Creating a new 'li' element for the toast
-    toast.className = `toast ${id}`; // Setting the classes for the toast
-    // Setting the inner HTML for the toast
-    toast.innerHTML = `<div class="column">
-                         <i class="fa-solid ${icon}"></i>
-                         <span>Your Message Successfully sent!</span>
-                      </div>
-                      <i class="fa-solid fa-xmark" onclick="removeToast(this.parentElement)"></i>`;
-    notifications.appendChild(toast); // Append the toast to the notification ul
-    // Setting a timeout to remove the toast after the specified duration
-    toast.timeoutId = setTimeout(() => removeToast(toast), toastDetails.timer);
-}
 
 // Adding a click event listener to each button to create a toast when clicked
-buttons.forEach(btn1 => {
-    form.addEventListener("submit", () => createToast(btn1.id));
-});
+// buttons.forEach(btn1 => {
+//     form.addEventListener("submit", () => createToast(btn1.id));
+// });
 
 // animation 
 
@@ -197,7 +178,7 @@ tl.from('.navitem', {
 // Typed Js 
 
 var typed = new Typed('#element', {
-  strings:['Frontend Developer', 'Web Develpoper','Web Designer'],
+  strings:['Frontend Developer', 'Web Developer','Web Designer'],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay:1000,
@@ -291,16 +272,26 @@ scrollLeft.forEach((el)=>observer.observe(el));
 
 // feedback star
 
-// const starbtn = document.querySelector("button");
+// const starbtn = document.querySelector(".btn-star");
 // const post = document.querySelector(".post");
 // const widget = document.querySelector(".star-widget");
-// const editBtn = document.querySelector(".edit");
 // starbtn.onclick = ()=>{
 //   widget.style.display = "none";
 //   post.style.display = "block";
-//   editBtn.onclick = ()=>{
-//     widget.style.display = "block";
-//     post.style.display = "none";
-//   }
 //   return false;
 // }
+
+var star = document.getElementById("star");
+var starbox = document.querySelector(".star-box");
+
+star.onclick =()=>{
+  if(starbox.style.display === "none"){
+    starbox.style.display = "block";
+  }
+  else{
+    starbox.style.display = "none";
+  }
+}
+
+
+

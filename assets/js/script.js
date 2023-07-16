3ebd4efaa33e2b3e64db17bf9607c79de684d0f7
Aditya Sahu <sahuaditya73@gmail.com>
2023-07-16 11:12:41 +0530

    added favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -288,3 +288,19 @@ scrollRight.forEach((el)=>observer.observe(el));
 
 const scrollLeft = document.querySelectorAll(".scroll-left");
 scrollLeft.forEach((el)=>observer.observe(el));
+
+// feedback star
+
+// const starbtn = document.querySelector("button");
+// const post = document.querySelector(".post");
+// const widget = document.querySelector(".star-widget");
+// const editBtn = document.querySelector(".edit");
+// starbtn.onclick = ()=>{
+//   widget.style.display = "none";
+//   post.style.display = "block";
+//   editBtn.onclick = ()=>{
+//     widget.style.display = "block";
+//     post.style.display = "none";
+//   }
+//   return false;
+// }
